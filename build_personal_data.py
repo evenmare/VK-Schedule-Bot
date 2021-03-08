9f6a5086fe31d3a7224526834d54302d3386b6ce
evenmare <evenmare@users.noreply.github.com>
2021-03-08 12:41:16 +0300

    minor updates

--- build_personal_data.py
+++ build_personal_data.py
@@ -1,7 +1,8 @@
-import mysql.connector as db
 from private import connectDB as connectDB
 
 class buildPersonalData:
+
+    tableName = "mainTable"
 
     dbConnection = connectDB()
 
@@ -13,7 +14,7 @@ class buildPersonalData:
 
     def selectUserInfo(self, perID):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "SELECT perID, name, eveningNotification, morningNotification, 30minNotification, 5minNotification, localLessons, distantLessons FROM mainTable WHERE perID = %s"
+        operationRequest = "SELECT perID, name, eveningNotification, morningNotification, 30minNotification, 5minNotification, localLessons, distantLessons FROM " + buildPersonalData.tableName + " WHERE perID = %s"
         operationValues = (perID, )
         operationCursor.execute(operationRequest, operationValues)
         operationResult = operationCursor.fetchone()
@@ -22,42 +23,42 @@ class buildPersonalData:
     def insertNewUserInfo(self, perID, name):
         operationCursor = self.dbConnection.cursor()
         self.deleteUserInfo(perID)
-        operationRequest = "INSERT INTO mainTable (perID, name, eveningNotification, morningNotification, 30minNotification, 5minNotification, localLessons, distantLessons) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        operationRequest = "INSERT INTO " + buildPersonalData.tableName + " (perID, name, eveningNotification, morningNotification, 30minNotification, 5minNotification, localLessons, distantLessons) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         operationValues = (perID, name, 0, 0, 0, 0, 0, 0)
         operationCursor.execute(operationRequest, operationValues)
         self.dbConnection.commit()
 
     def updateUserInfo(self, perID, eveningNotification, morningNotification, min30Notification, min5Notification, localLessons, distantLessons):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "UPDATE mainTable SET eveningNotification = %s, morningNotification = %s, 30minNotification = %s, 5minNotification = %s, localLessons = %s, distantLessons = %s WHERE perID = %s"
+        operationRequest = "UPDATE " + buildPersonalData.tableName + " SET eveningNotification = %s, morningNotification = %s, 30minNotification = %s, 5minNotification = %s, localLessons = %s, distantLessons = %s WHERE perID = %s"
         operationValues = (eveningNotification, morningNotification, min30Notification, min5Notification, localLessons, distantLessons, perID)
         operationCursor.execute(operationRequest, operationValues)
         self.dbConnection.commit()
 
     def deleteUserInfo(self, perID):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "DELETE FROM mainTable WHERE perID = %s"
+        operationRequest = "DELETE FROM " + buildPersonalData.tableName + " WHERE perID = %s"
         operationValues = (perID, )
         operationCursor.execute(operationRequest, operationValues)
         self.dbConnection.commit()
 
     def selectAllUsers(self):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "SELECT perID FROM mainTable"
+        operationRequest = "SELECT perID FROM " + buildPersonalData.tableName
         operationCursor.execute(operationRequest)
         operationResult = operationCursor.fetchall()
         return self.formattingSelectQuery(operationResult)
 
     def selectTimeNotifications(self, time_type_of_notification):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "SELECT perID FROM mainTable WHERE " + time_type_of_notification + "Notification = 1"
+        operationRequest = "SELECT perID FROM " + buildPersonalData.tableName + " WHERE " + time_type_of_notification + "Notification = 1"
         operationCursor.execute(operationRequest)
         operationResult = operationCursor.fetchall()
         return self.formattingSelectQuery(operationResult)
 
     def selectLocationNotifications(self, location_type_of_notification):
         operationCursor = self.dbConnection.cursor()
-        operationRequest = "SELECT perID FROM mainTable WHERE " + location_type_of_notification + "Lessons = 1"
+        operationRequest = "SELECT perID FROM " + buildPersonalData.tableName + " WHERE " + location_type_of_notification + "Lessons = 1"
         operationCursor.execute(operationRequest)
         operationResult = operationCursor.fetchall()
         return self.formattingSelectQuery(operationResult)
